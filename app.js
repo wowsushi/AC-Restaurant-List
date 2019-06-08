be265410c8e8626ab67520d3910c0bfff49834dd
bettyChen <s9654025@gmail.com>
2019-06-08 22:17:48 +0800

    create index and show page

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurantList.results.find( restaurant => {
+    return restaurant.id.toString() === id
+  })
+
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const matchedRestaurant = restaurantList.results.filter( restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: matchedRestaurant, keyword: keyword })
+})
+
+app.listen(port, () => {
+  console.log('running success!')
+})
---
+++ public/javascripts/main.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
